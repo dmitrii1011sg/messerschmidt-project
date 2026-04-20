a48dba64e10f3a51e8c1ffaf399dedd0d5e9ef99
Gorshkov Dmitrii <dmitrii1011sg@gmail.com>
2026-04-20 13:46:56 +0300

    deploy

--- libs/msdt-map-engine/src/lib/msdt-map.component.ts
+++ libs/msdt-map-engine/src/lib/msdt-map.component.ts
@@ -8,9 +8,10 @@ import {
   MapComponent as MglMap,
   RasterSourceComponent,
   LayerComponent,
+  RasterDemSourceComponent,
 } from '@maplibre/ngx-maplibre-gl';
 import { MsdtMapService } from './services/msdt-map.service';
-import { LngLatBounds } from 'maplibre-gl';
+import { LngLatBounds, Map } from 'maplibre-gl';
 import {
   MsdtMapMarkerComponent,
   MsdtPoint,
@@ -22,18 +23,20 @@ import {
   imports: [
     MglMap,
     RasterSourceComponent,
+    RasterDemSourceComponent,
     LayerComponent,
     MsdtMapMarkerComponent,
   ],
   template: `
     <mgl-map
-      [mapStyle]="mapLibreExampleSource"
+      [mapStyle]="mapLibreSource"
       [zoom]="[4]"
       [center]="[129.7, 62.0]"
       [pitch]="[45]"
       [canvasContextAttributes]="{ preserveDrawingBuffer: true }"
       [maxBounds]="bounds"
       class="absolute inset-0 h-full w-full bg-slate-100"
+      (mapLoad)="onMapLoad($event)"
     >
       <mgl-raster-source
         id="messerschmidt-source"
@@ -41,6 +44,12 @@ import {
         [tiles]="['https://mapwarper.net/mosaics/tile/2476/{z}/{x}/{y}.png']"
         [tileSize]="256"
       ></mgl-raster-source>
+
+      <mgl-raster-dem-source
+        id="terrain-source"
+        [tiles]="[mapLibreTerrainSource]"
+        [tileSize]="256"
+      ></mgl-raster-dem-source>
 
       <mgl-layer
         id="messerschmidt-layer"
@@ -75,23 +84,30 @@ export class MsdtMapComponent {
     [109.3616, 58.3127],
   );
 
-  readonly mapLibreExampleSource: string =
-    'https://api.maptiler.com/maps/hybrid/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL';
+  readonly mapLibreSource: string = `https://api.maptiler.com/maps/hybrid/style.json?key=${(import.meta as any).env.NG_APP_MAPTILER_KEY}`;
+  readonly mapLibreTerrainSource: string = `https://api.maptiler.com/tiles/terrain-rgb-v2/{z}/{x}/{y}.webp?key=${(import.meta as any).env.NG_APP_MAPTILER_KEY}`;
 
   protected readonly stops = signal<MsdtPoint[]>([
     {
       id: 1,
       name: 'Красноярово',
       coordinates: [107.4565, 57.3375],
-      description: 'Здесь Мессершмидт изучал...',
+      description: 'Красноярово',
     },
     {
       id: 2,
       name: 'Макарово',
       coordinates: [107.8231, 57.4835],
-      description: 'Наблюдения за скалами...',
+      description: 'Макарово',
     },
   ]);
+
+  onMapLoad(map: Map): void {
+    map.setTerrain({
+      source: 'terrain-source',
+      exaggeration: 1.8,
+    });
+  }
 
   onMarkerClick(stop: MsdtPoint): void {
     console.warn(`Нажата остановка: ${stop.name}`);
